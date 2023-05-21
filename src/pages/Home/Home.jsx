09807149fc3e3aba63618192dd00cadf00ba75b6
dankorotenko <danilkorotenko2@gmail.com>
2023-05-21 22:20:16 +0200

    basic setup

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import "./Home.scss";
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <section className="home">
+        Home
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
