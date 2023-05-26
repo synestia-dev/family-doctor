b46d29a1f90dddd13da5a42488a405a82907bb36
dankorotenko <danilkorotenko2@gmail.com>
2023-05-26 20:48:22 +0200

    preheader translated

--- src/components/Header/Preheader/Preheader.jsx
+++ src/components/Header/Preheader/Preheader.jsx
@@ -2,14 +2,16 @@ import "./Preheader.scss";
 import geo from "../../../assets/icons/geo.svg";
 import phone from "../../../assets/icons/phone.svg";
 import mail from "../../../assets/icons/mail.svg";
+import { useTranslation } from "react-i18next";
 const Preheader = () => {
+  const {t} = useTranslation()
   return (
     <div className="preheader">
       <div className="container">
         <div className="preheader__inner">
           <div className="preheader__left">
             <img src={geo} alt="geo" />
-            <p>м. Запоріжжя, вул. Лікарняна, 18.</p>
+            <p>{t('м. Запоріжжя, вул. Лікарняна, 18')}</p>
           </div>
           <div className="preheader__right">
             <div>
