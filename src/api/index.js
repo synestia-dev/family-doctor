65cf2941579d031b80899a4d801841f377da4f9b
dankorotenko <danilkorotenko2@gmail.com>
2023-05-26 21:47:04 +0200

    test

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ export async function getRequest(url) {
   try {
     const response = await fetch(`${import.meta.env.VITE_API_URL}/api/${url}`);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     return data;
   } catch (error) {
     console.log("Error fetching data:", error);
