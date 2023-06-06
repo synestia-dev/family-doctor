0db9c782a578f76ecb6bf8222fdf69aefa0f46d2
Rostislav Martinyuk <kievstarpro01082017@gmail.com>
2023-06-07 01:05:36 +0300

    implemented for companies page

--- src/pages/ForCompanies/ForCompanies.jsx
+++ src/pages/ForCompanies/ForCompanies.jsx
@@ -1,7 +1,46 @@
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 import "./ForCompanies.scss";
+import itemIcon from "../../assets/icons/forCompaniesIcon.svg";
 import { useLayoutEffect } from "react";
+
+const items = [
+  {
+    id: 1,
+    title: "Профілактичні огляди робітників згідно наказів МОЗ №246 та №280",
+    image: itemIcon,
+  },
+  {
+    id: 2,
+    title: "Профілактичні огляди робітників згідно наказів МОЗ №246 та №280",
+    image: itemIcon,
+  },
+  {
+    id: 3,
+    title: "Профілактичні огляди робітників згідно наказів МОЗ №246 та №280",
+    image: itemIcon,
+  },
+  {
+    id: 4,
+    title: "Профілактичні огляди робітників згідно наказів МОЗ №246 та №280",
+    image: itemIcon,
+  },
+  {
+    id: 5,
+    title: "Профілактичні огляди робітників згідно наказів МОЗ №246 та №280",
+    image: itemIcon,
+  },
+  {
+    id: 6,
+    title: "Профілактичні огляди робітників згідно наказів МОЗ №246 та №280",
+    image: itemIcon,
+  },
+  {
+    id: 7,
+    title: "Профілактичні огляди робітників згідно наказів МОЗ №246 та №280",
+    image: itemIcon,
+  },
+];
 const ForCompanies = () => {
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
@@ -10,7 +49,28 @@ const ForCompanies = () => {
     <>
       <Header />
       <section className="forCompanies">
-        <div className="container">ForCompanies</div>
+        <div className="container">
+          <h2>Для компаній</h2>
+          <div className="forCompanies__list">
+            {items.map((item) => (
+              <div className="forCompanies__list_item" key={item.id}>
+                <img src={item.image} alt="icon" />
+                <p>{item.title}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="forCompanies__feedback">
+          <div className="container">
+            <div className="block">
+              <h3>У вас є питання щодо медичної пропозиції для компаній?</h3>
+              <div>
+                <p>Напишіть нам</p>
+                <a href="mailto:Shyroke_likar@ukr.net">Shyroke_likar@ukr.net</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       <Footer />
     </>
