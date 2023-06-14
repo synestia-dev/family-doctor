bc186c9c2796cb1168b16427f275ededde2f5baa
Adamovproga2003 <gutrfknc@gmail.com>
2023-06-14 17:21:21 +0300

    Changed advantages text

--- src/pages/Home/components/OurAdvantages/AdvantageItem/AdvantageItem.jsx
+++ src/pages/Home/components/OurAdvantages/AdvantageItem/AdvantageItem.jsx
@@ -1,23 +1,23 @@
-import "./AdvantageItem.scss";
-import PropTypes from "prop-types";
-import FamilyIcon from "../../../../../assets/iconComponents/FamilyIcon.jsx";
+import './AdvantageItem.scss'
+import PropTypes from 'prop-types'
+import FamilyIcon from '../../../../../assets/iconComponents/FamilyIcon.jsx'
 const AdvantageItem = ({ title, text }) => {
-  return (
-    <div className="advantage">
-      <div className="advantage__circle">
-        <FamilyIcon />
-      </div>
-      <div className="advantage__info">
-        <h5 className="advantage__title">{title}</h5>
-        <p className="advantage__text">{text}</p>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className='advantage'>
+			<div className='advantage__circle'>
+				<FamilyIcon />
+			</div>
+			<div className='advantage__info'>
+				<h5 className='advantage__title'>{title}</h5>
+				{/* <p className='advantage__text'>{title}</p> */}
+			</div>
+		</div>
+	)
+}
 
 AdvantageItem.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string.isRequired,
-};
+	title: PropTypes.string.isRequired,
+	text: PropTypes.string.isRequired,
+}
 
-export default AdvantageItem;
+export default AdvantageItem
