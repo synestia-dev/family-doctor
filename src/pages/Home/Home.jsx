6d557456fc8da0a7ae9eccedabf7a7073361ef7d
Adamovproga2003 <gutrfknc@gmail.com>
2023-06-22 13:39:52 +0300

    Translate (Main Page) En, De

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,38 +1,39 @@
-import Footer from "../../components/Footer/Footer";
-import Header from "../../components/Header/Header";
-import Banner from "../../pages/Home/components/Banner/Banner.jsx";
-import "./Home.scss";
-import Offer from "./components/Offer/Offer.jsx";
-import OurAdvantages from "../../pages/Home/components/OurAdvantages/OurAdvantages.jsx";
-import horizontalLogo from "../../assets/icons/horizontal-logo.svg";
-import NewsList from "../../components/NewsList/NewsList.jsx";
-import Button from "../../components/Button/Button.jsx";
-import { useLayoutEffect } from "react";
+import Footer from '../../components/Footer/Footer'
+import Header from '../../components/Header/Header'
+import Banner from '../../pages/Home/components/Banner/Banner.jsx'
+import './Home.scss'
+import Offer from './components/Offer/Offer.jsx'
+import OurAdvantages from '../../pages/Home/components/OurAdvantages/OurAdvantages.jsx'
+import horizontalLogo from '../../assets/icons/horizontal-logo.svg'
+import NewsList from '../../components/NewsList/NewsList.jsx'
+import Button from '../../components/Button/Button.jsx'
+import { useLayoutEffect } from 'react'
 const Home = () => {
-  useLayoutEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  return (
-    <>
-      <Header />
-      <section className="home">
-        <Banner />
-        <Offer />
-        <OurAdvantages />
-        <div className="container">
-          <img
-            src={horizontalLogo}
-            alt={horizontalLogo}
-            className="horizontal-logo"
-          />
-        </div>
+	useLayoutEffect(() => {
+		window.scrollTo(0, 0)
+	}, [])
 
-        <NewsList count={6} />
-        <Button to="/news" text="Усі наші статті" color="articles" />
-      </section>
-      <Footer />
-    </>
-  );
-};
+	return (
+		<>
+			<Header />
+			<section className='home'>
+				<Banner />
+				<Offer />
+				<OurAdvantages />
+				<div className='container'>
+					<img
+						src={horizontalLogo}
+						alt={horizontalLogo}
+						className='horizontal-logo'
+					/>
+				</div>
 
-export default Home;
+				<NewsList count={6} />
+				<Button to='/news' text='Усі наші статті' color='articles' />
+			</section>
+			<Footer />
+		</>
+	)
+}
+
+export default Home
--- src/pages/Home/components/OurAdvantages/AdvantageItem/AdvantageItem.jsx
+++ src/pages/Home/components/OurAdvantages/AdvantageItem/AdvantageItem.jsx
@@ -1,14 +1,16 @@
 import './AdvantageItem.scss'
 import PropTypes from 'prop-types'
 import FamilyIcon from '../../../../../assets/iconComponents/FamilyIcon.jsx'
+import { useTranslation } from 'react-i18next'
 const AdvantageItem = ({ title, text }) => {
+	const { t } = useTranslation()
 	return (
 		<div className='advantage'>
 			<div className='advantage__circle'>
 				<FamilyIcon />
 			</div>
 			<div className='advantage__info'>
-				<h5 className='advantage__title'>{title}</h5>
+				<h5 className='advantage__title'>{t(title)}</h5>
 				{/* <p className='advantage__text'>{title}</p> */}
 			</div>
 		</div>
